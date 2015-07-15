75a509eed36681f231b30d58842cb27e632fe37e
Joao Pedro Dias <jpdias@live.com.pt>
2015-07-15 01:55:44 +0100

    removed awaits from slickpersistence, some pending fixes

--- src/main/scala/eu/shiftforward/persistence/SlickPersistenceActor.scala
+++ src/main/scala/eu/shiftforward/persistence/SlickPersistenceActor.scala
@@ -18,13 +18,14 @@ object SlickPersistenceActor extends LazyLogging {
   val deploys = TableQuery[Deploys]
   val events = TableQuery[Events]
 
-  var tables = Await.result(db.run(MTable.getTables), 1.seconds).toList
-  //if(tables.size <2){
-
   logger.info("Updating db.")
   val ddl = projects.schema ++ deploys.schema ++ events.schema
-  db.run(ddl.drop)
-  db.run(ddl.create)
+  db.run(DBIO.seq(
+    ddl.drop,
+    ddl.create
+  ))
+  //Await.result(db.run(ddl.drop), 5.seconds)
+  // Await.result(db.run(ddl.create), 5.seconds)
   //}
 }
 
@@ -34,56 +35,65 @@ class SlickPersistenceActor extends PersistenceActor {
 
   override implicit def ec: ExecutionContext = context.dispatcher
 
-  override def addDeploy(name: String, deploy: RequestDeploy): Future[Option[Deploy]] = Future {
-    Await.result(db.run(projects.filter(_.name === name).result.headOption), 5.seconds).map { p =>
-      val newDeploy = DeployModel(
-        UUID.randomUUID().toString,
-        deploy.user,
-        currentTime,
-        deploy.commit.branch,
-        deploy.commit.hash,
-        deploy.description,
-        deploy.changelog,
-        deploy.version,
-        deploy.isAutomatic,
-        deploy.client,
-        name
-      )
-      db.run(deploys += newDeploy)
-      Await.result(db.run(events += EventModel(currentTime, DeployStatus.Started, "", newDeploy.id)), 5.seconds)
-      val deployEvents = Await.result(db.run(events.filter(_.deployID === newDeploy.id).result), 5.seconds).map { e =>
-        Event(e.timestamp, e.status, e.description)
-      }.toList
-      Deploy(
-        newDeploy.user,
-        newDeploy.timestamp,
-        Commit(newDeploy.commit_hash, newDeploy.commit_branch),
-        newDeploy.description,
-        deployEvents,
-        newDeploy.changelog,
-        newDeploy.id,
-        newDeploy.version,
-        newDeploy.isAutomatic,
-        newDeploy.client
-      )
+  override def addDeploy(name: String, deploy: RequestDeploy): Future[Option[Deploy]] = {
+    db.run(projects.filter(_.name === name).result.headOption).flatMap { projOpt =>
+      val z = projOpt.map { p =>
+        val newDeploy = DeployModel(
+          UUID.randomUUID().toString,
+          deploy.user,
+          currentTime,
+          deploy.commit.branch,
+          deploy.commit.hash,
+          deploy.description,
+          deploy.changelog,
+          deploy.version,
+          deploy.isAutomatic,
+          deploy.client,
+          name
+        )
+        val x = db.run(deploys += newDeploy)
+        val deployEvent = EventModel(currentTime, DeployStatus.Started, "", newDeploy.id)
+        val y = db.run(events += deployEvent)
+        val o = x.zip(y).map {
+          case _ =>
+            Some(Deploy(
+              newDeploy.user,
+              newDeploy.timestamp,
+              Commit(newDeploy.commit_hash, newDeploy.commit_branch),
+              newDeploy.description,
+              List(Event(deployEvent.timestamp, deployEvent.status, deployEvent.description)),
+              newDeploy.changelog,
+              newDeploy.id,
+              newDeploy.version,
+              newDeploy.isAutomatic,
+              newDeploy.client
+            ))
+        }
+        o
+      }.getOrElse(Future.successful(None))
+      z
     }
   }
 
-  override def deleteProject(name: String): Future[Option[ResponseProject]] = Future {
-    Await.result(db.run(projects.filter(_.name === name).result.headOption), 5.seconds).map { p =>
+  override def deleteProject(name: String): Future[Option[ResponseProject]] = {
+    db.run(projects.filter(_.name === name).result.headOption).map { list =>
       db.run(projects.filter(_.name === name).delete)
-      ResponseProject(p.name, p.description, p.createdAt, p.git)
+      list.map { p =>
+        ResponseProject(p.name, p.description, p.createdAt, p.git)
+      }
     }
   }
 
-  override def getProject(name: String): Future[Option[ResponseProject]] = Future {
-    Await.result(db.run(projects.filter(_.name === name).result.headOption), 5.seconds).map { p =>
-      ResponseProject(p.name, p.description, p.createdAt, p.git)
+  override def getProject(name: String): Future[Option[ResponseProject]] = {
+    db.run(projects.filter(_.name === name).result.headOption).map { h =>
+      h.map { p =>
+        ResponseProject(p.name, p.description, p.createdAt, p.git)
+      }
     }
   }
 
-  override def addEvent(projName: String, deployId: String, event: RequestEvent): Future[Option[Event]] = Future {
-    Await.result(db.run(projects.filter(_.name === projName).result.headOption), 5.seconds) match {
+  override def addEvent(projName: String, deployId: String, event: RequestEvent): Future[Option[Event]] = {
+    db.run(projects.filter(_.name === projName).result.headOption).map {
       case Some(_) => Some({
         val newEvent = EventModel(currentTime, DeployStatus.Started, "", deployId)
         db.run(events += newEvent)
@@ -92,10 +102,9 @@ class SlickPersistenceActor extends PersistenceActor {
     }
   }
 
-  override def saveProject(project: RequestProject): Future[ResponseProject] = Future {
+  override def saveProject(project: RequestProject): Future[ResponseProject] = {
     val proj = ProjectModel(project.name, project.description, currentTime, project.git)
-
-    Await.result(db.run(projects.filter(_.name === proj.name).result.headOption), 5.seconds) match {
+    db.run(projects.filter(_.name === proj.name).result.headOption).map {
       case Some(_) => throw new DuplicatedEntry(proj.name + " already exists.")
       case None => {
         db.run(projects += proj)
@@ -104,49 +113,69 @@ class SlickPersistenceActor extends PersistenceActor {
     }
   }
 
-  override def getDeploys(name: String, max: Int): Future[Option[List[Deploy]]] = Future {
-    Some(Await.result(db.run(deploys.filter(_.projName === name).result), 5.seconds).map { d =>
-      val deployEvents = Await.result(db.run(events.filter(_.deployID === d.id).result), 5.seconds).map { e =>
+  def getEvents(id: String): Future[List[Event]] ={
+    db.run(events.filter(_.deployID === id).result).map { _.map { e =>
         Event(e.timestamp, e.status, e.description)
       }.toList
-      Deploy(
-        d.user,
-        d.timestamp,
-        Commit(d.commit_hash, d.commit_branch),
-        d.description,
-        deployEvents,
-        d.changelog,
-        d.id,
-        d.version,
-        d.isAutomatic,
-        d.client
+    }
+  }
+
+  def projExists(name: String): Future[Boolean] = {
+    db.run(projects.filter(_.name === name).result).map{ f => f.nonEmpty }
+  }
+
+  /*
+  *   Deploy(
+              d.user,
+              d.timestamp,
+              Commit(d.commit_hash, d.commit_branch),
+              d.description,
+              f,
+              d.changelog,
+              d.id,
+              d.version,
+              d.isAutomatic,
+              d.client
+            )
+            : Future[Option[List[Deploy]]]
+  * */
+  override def getDeploys(name: String, max: Int): Future[Option[List[Deploy]]] = ??? /* {
+  db.run(deploys.filter(_.projName === name).result).map(f =>
+      Future.sequence(
+        f.map { d =>
+          getEvents(d.id).map(f => Deploy(d.user, d.timestamp, Commit(d.commit_hash, d.commit_branch), d.description, f, d.changelog, d.id, d.version, d.isAutomatic, d.client))
+        }.toList.take(max)
       )
-    }.toList.take(max))
-  }
+    )
+  }*/
 
-  override def getProjects: Future[List[ResponseProject]] = Future {
-    Await.result(db.run(projects.result), 5.seconds).map { p =>
-      ResponseProject(p.name, p.description, p.createdAt, p.git)
-    }.toList
-  }
-
-  override def getDeploy(projName: String, deployId: String): Future[Option[Deploy]] = Future {
-    Await.result(db.run(deploys.filter(_.projName === projName).filter(_.id === deployId).result.headOption), 5.seconds).map { d =>
-      val deployEvents = Await.result(db.run(events.filter(_.deployID === d.id).result), 5.seconds).map { e =>
-        Event(e.timestamp, e.status, e.description)
+  override def getProjects: Future[List[ResponseProject]] = {
+    db.run(projects.result).map { list =>
+      list.map { p =>
+        ResponseProject(p.name, p.description, p.createdAt, p.git)
       }.toList
-      Deploy(
-        d.user,
-        d.timestamp,
-        Commit(d.commit_hash, d.commit_branch),
-        d.description,
-        deployEvents,
-        d.changelog,
-        d.id,
-        d.version,
-        d.isAutomatic,
-        d.client
-      )
+    }
+  }
+
+  override def getDeploy(projName: String, deployId: String): Future[Option[Deploy]] = {
+    db.run(deploys.filter(_.projName === projName).filter(_.id === deployId).result.headOption).map { list =>
+      list.map { d =>
+        val deployEvents = Await.result(db.run(events.filter(_.deployID === d.id).result), 5.seconds).map { e =>
+          Event(e.timestamp, e.status, e.description)
+        }.toList
+        Deploy(
+          d.user,
+          d.timestamp,
+          Commit(d.commit_hash, d.commit_branch),
+          d.description,
+          deployEvents,
+          d.changelog,
+          d.id,
+          d.version,
+          d.isAutomatic,
+          d.client
+        )
+      }
     }
   }
 }

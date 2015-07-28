34f00798007c74b8e735be29400ce403e6f7a1bb
Joao Pedro Dias <jpdias@live.com.pt>
2015-07-28 10:39:35 +0100

    add sample random

--- DeployScript/sample.rb
+++ DeployScript/sample.rb
@@ -10,6 +10,18 @@ if __FILE__ == $0
   puts p1.add_deploy('Last deploy version', 'https://bitbucket.org/shiftforward/malamute', 'v0.1', false, 'none')
   puts p2.add_deploy('Last deploy version', 'https://bitbucket.org/shiftforward/adstax', 'v0.1', false, 'xpto')
   sleep 2
-  n = rand(0..2)
-
+  n = rand(0..3)
+  if n == 0
+    puts p1.add_deploy_event(DeployStatus::FAILED, "Done.")
+    puts p2.add_deploy_event(DeployStatus::SUCCESS, "Done.")
+  elsif n == 1
+    puts p1.add_deploy_event(DeployStatus::SUCCESS, "Done.")
+    puts p2.add_deploy_event(DeployStatus::FAILED, "Done.")
+  elsif n == 2
+    puts p1.add_deploy_event(DeployStatus::LOG, "Information.")
+    puts p2.add_deploy_event(DeployStatus::LOG, "Information.")
+  else
+    puts p1.add_deploy_event(DeployStatus::SKIPPED, "Done.")
+    puts p2.add_deploy_event(DeployStatus::SKIPPED, "Done.")
+  end
 end

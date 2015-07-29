c8d569d1bb93f1ea2112905ba2d138cdf36080ba
Joao Pedro Dias <jpdias@live.com.pt>
2015-07-29 17:41:41 +0100

    add test to get clients route

--- src/test/scala/eu/shiftforward/deploylogger/DeployLoggerRouteSpec.scala
+++ src/test/scala/eu/shiftforward/deploylogger/DeployLoggerRouteSpec.scala
@@ -346,6 +346,37 @@ class DeployLoggerRouteSpec extends Specification with Specs2RouteTest {
         }
       }
     }
+    "handle /project/:name/clients" in {
+      "return a 'JSON list string' response for GET requests" in new MockDeployLoggerService {
+        Post("/project",
+          RequestProject("TestProj", "Proj Description Test", "http://bitbucket.com/abc")
+        ) ~> projectPostRoute ~> check {
+          status === OK
+          responseAs[ResponseProject].name must beEqualTo("TestProj")
+          responseAs[ResponseProject].description must beEqualTo("Proj Description Test")
+        }
+        eventually {
+          Post("/project/TestProj/deploy",
+            RequestDeploy("testUser",
+              Commit("abc124ada", "master"), "testestess", "http://google.com/", "1.1.1", false, "Cliente",
+              List(
+                RequestModule("ModuleX","v0.1",ModuleStatus.Add),
+                RequestModule("ModuleX","v0.1",ModuleStatus.Remove),
+                RequestModule("ModuleX","v0.2",ModuleStatus.Add)
+              ), "This config")
+          ) ~> projectDeployPostRoute ~> check {
+            status === OK
+            responseAs[ResponseDeploy].user must beEqualTo("testUser")
+
+            Get("/project/TestProj/clients") ~> projectGetClients ~> check {
+              status === OK
+              import spray.json.DefaultJsonProtocol._
+              responseAs[List[String]].head === "Cliente"
+            }
+          }
+        }
+      }
+    }
   }
   step {
     import java.io._

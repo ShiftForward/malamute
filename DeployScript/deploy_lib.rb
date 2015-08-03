7e9b7193afc30c2d27d04e292c09c95f98c29294
jpdias <jpdias@live.com.pt>
2015-08-03 18:40:32 +0100

    added deploy objects

--- DeployScript/deploy_lib.rb
+++ DeployScript/deploy_lib.rb
@@ -21,8 +21,6 @@ class Project
 
   def initialize(project_name)
     @project_name = project_name
-    @last_deploy_id = ""
-    @modules = Array.new
   end
 
   def self.new_project(name, description, git)
@@ -47,18 +45,7 @@ class Project
       raise "Code: #{res.code} : #{res.body}"
     end
   end
-
-  def self.get_projects
-    uri = URI.parse(URL + "projects")
-
-    req = Net::HTTP::Get.new(uri.request_uri)
-
-    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-      http.request(req)
-    end
-    "Code: #{res.code} : #{res.body}"
-  end
-
+  
   def self.open_project(name)
     uri = URI.parse(URL + "project/#{name}")
 
@@ -74,8 +61,28 @@ class Project
       raise "Code: #{res.code} : #{res.body}"
     end
   end
+  
+  def new_deploy(description,changelog,version,automatic,client,configuration)
+      Deploy.new(description,changelog,version,automatic,client,configuration,@project_name)
+  end
+  
+end
 
-  def start_deploy(description, changelog, version, automatic, client, config)
+class Deploy
+
+  def initialize(description,changelog,version,automatic,client,configuration,project_name)
+    @description = description
+    @changelog = changelog
+    @version = version
+    @modules = Array.new
+    @configuration = configuration
+    @automatic = automatic
+    @client = client
+    @last_deploy_id = "" 
+    @project_name = project_name
+  end
+  
+  def start_deploy()
     #git configurations for current folder
     user =  `git config --get user.name`.chomp!
     commit_branch = `git rev-parse --abbrev-ref HEAD`.chomp!
@@ -92,13 +99,13 @@ class Project
             branch: commit_branch,
             hash: commit_hash
         },
-        description: description,
-        changelog: changelog,
-        version: version,
-        automatic: automatic,
-        client: client,
+        description: @description,
+        changelog: @changelog,
+        version: @version,
+        automatic: @automatic,
+        client: @client,
         modules: @modules,
-        configuration: config
+        configuration: @configuration
     }.to_json
 
     res = Net::HTTP.start(uri.hostname, uri.port) do |http|
@@ -110,11 +117,7 @@ class Project
       raise "Code: #{res.code} : #{res.body}"
     end
   end
-
-  def with_module(name,version,status)
-    @modules.push({:name => "#{name}", :version =>  "#{version}", :status =>  "#{status}"})
-  end
-
+  
   def add_deploy_event(status, description)
     uri = URI.parse(URL + "project/#{@project_name}/deploy/#{@last_deploy_id}/event")
 
@@ -134,6 +137,42 @@ class Project
     else
       raise "Code: #{res.code} : #{res.body}"
     end
-
   end
+    
+  def with_module(name,version,status)
+    @modules.push({:name => "#{name}", :version =>  "#{version}", :status =>  "#{status}"})
+  end
+  
+  def with_modules(modules)
+    modules.each do |mod|
+      @modules.push({:name => "#{mod[0]}", :version =>  "#{mod[1]}", :status =>  "#{mod[2]}"})
+    end
+  end
+  
+  def with_client(client_name)
+    @client = client_name
+    @modules = Array.new
+  end
+  
+  def add_deploy_event(status, description)
+    uri = URI.parse(URL + "project/#{@project_name}/deploy/#{@last_deploy_id}/event")
+
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req['Content-Type'] = 'application/json'
+
+    req.body = {
+        status: status,
+        description: description
+    }.to_json
+
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+    if res.kind_of?(Net::HTTPSuccess)
+      res.body
+    else
+      raise "Code: #{res.code} : #{res.body}"
+    end
+  end
+  
 end
--- DeployScript/sample.rb
+++ DeployScript/sample.rb
@@ -2,32 +2,40 @@ require_relative 'deploy_lib'
 
 if __FILE__ == $0
   p1 = Project.open_project('malamute')
-  p2 = Project.open_project('adstax')
-  #p1 = Project.new_project('malamute','Deploy Logger Service','https://bitbucket.org/shiftforward/malamute')
-  #p2 = Project.new_project('adstax','adstax','https://bitbucket.org/shiftforward/adstax')
-  sleep 5
-  #p1.with_module("AdTracker","SNAPSHOT 1.0",ModuleStatus::ADD)
-  p1.with_module("AdTracker","SNAPSHOT 2.0",ModuleStatus::ADD)
-  p2.with_module("RegisterTracker","SNAPSHOT 1.0",ModuleStatus::REMOVE)
-  puts Project.get_projects
-  puts p1.start_deploy('Last deploy version', 'https://bitbucket.org/shiftforward/malamute', 'v0.1', false, 'KuantoKusta', "This config")
-  sleep 5
-  puts p1.add_deploy_event(DeployStatus::LOG, "Information.")
-  puts p2.start_deploy('Last deploy version', 'https://bitbucket.org/shiftforward/adstax', 'v0.1', false, 'FEUP', "This config")
-  sleep 5
-  puts p2.add_deploy_event(DeployStatus::LOG, "Information.")
+
+  d1 = p1.new_deploy('TESTESTE version', 'https://bitbucket.org/shiftforward/malamute',
+                     'v0.2132', false, 'KuantoKusta', "This config")
+
+  puts d1.with_module("ABCD!","SNAPSHOT 2.0",ModuleStatus::ADD)
+
+  puts d1.start_deploy()
+
+  puts d1.add_deploy_event(DeployStatus::LOG, "Information.")
   n = rand(0..2)
   if n == 0
-    puts p1.add_deploy_event(DeployStatus::FAILED, "Done.")
-    sleep 1
-    puts p2.add_deploy_event(DeployStatus::SUCCESS, "Done.")
+    puts d1.add_deploy_event(DeployStatus::FAILED, "Failed.")
   elsif n == 1
-    puts p1.add_deploy_event(DeployStatus::SUCCESS, "Done.")
-    sleep 1
-    puts p2.add_deploy_event(DeployStatus::FAILED, "Done.")
+    puts d1.add_deploy_event(DeployStatus::SUCCESS, "Success.")
   else
-    puts p1.add_deploy_event(DeployStatus::SKIPPED, "Done.")
-    sleep 1
-    puts p2.add_deploy_event(DeployStatus::SKIPPED, "Done.")
+    puts d1.add_deploy_event(DeployStatus::SKIPPED, "Skipped.")
+  end
+    
+  d1.with_client("BlaBlaXPTO")
+  puts d1.with_modules([
+    ["ADxda","SNAPSHOT 2.0",ModuleStatus::ADD],
+    ["xptox","SNAPSHOT 2.1",ModuleStatus::ADD],
+    ["xyz","SNAPSHOT 2.1a",ModuleStatus::ADD]
+  ])
+  d1.start_deploy()
+
+  
+  puts d1.add_deploy_event(DeployStatus::LOG, "Information.")
+  n = rand(0..2)
+  if n == 0
+    puts d1.add_deploy_event(DeployStatus::FAILED, "Failed.")
+  elsif n == 1
+    puts d1.add_deploy_event(DeployStatus::SUCCESS, "Success.")
+  else
+    puts d1.add_deploy_event(DeployStatus::SKIPPED, "Skipped.")
   end
 end
